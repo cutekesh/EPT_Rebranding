cd3a4e1df55436421979aa06b3ccf8e90c197824
CK <cutekesh.21@gmail.com>
2025-06-27 06:15:12 -0700

    updated project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import { useState } from "react";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
--- src/pages/Home/Sections/WhyChooseUs/EPTAdvantage.jsx
+++ src/pages/Home/Sections/WhyChooseUs/EPTAdvantage.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const EPTAdvantage = () => {
-  return <div>EPTAdvantage</div>;
+  return (
+    <div>
+      <p className="text-white">WHY CHOOSE US</p>
+      <h2>The EPT Advantage</h2>
+      <div className="">
+        <div className="">
+          <h3 className="">We Plan & Design</h3>
+          <p className="">Smart blueprints for oil, gas, and power systems.</p>
+          <Link className="">
+            Learn More <img src="" alt="" />
+          </Link>
+        </div>
+        <div className="">
+          <h3>We Build & Deliver</h3>
+          <p>From foundation to finish, we build safely and fast</p>
+          <Link>
+            Learn More <img src="" alt="" />
+          </Link>
+        </div>
+        <div className="">
+          <h3>We Equip & Support</h3>
+          <p>Rent or buy equipment, get project support no delays</p>
+          <Link>
+            Learn More <img src="" alt="" />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default EPTAdvantage;
