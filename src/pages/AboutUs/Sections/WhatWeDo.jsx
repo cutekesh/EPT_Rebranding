6800558c50b3f58889908f96d4bbd1d0a207ee1f
WONDER SMG <mac@macs-MacBook-Pro-3.local>
2025-07-06 11:04:41 +0100

    created a WhatWeDo.jsx component to cater for the implementation of What We Do and Our Clients Enjoy sections of the About Us page

---
+++ src/pages/AboutUs/Sections/WhatWeDo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WhatWeDo = () => {
+  return (
+<main>
+    <section className='text-black'>WHAT WE DO</section>
+    <section className='text-black'>OUR CLIENTS ENJOY</section>
+</main>  )
+}
+
+export default WhatWeDo
