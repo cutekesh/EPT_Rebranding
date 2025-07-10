49eb897a952b97daa2ae1682955929930487b9bd
CK <cutekesh.21@gmail.com>
2025-07-10 05:00:33 -0700

    updates

--- src/pages/Home/Sections/WhatWeDo/Expertise.jsx
+++ src/pages/Home/Sections/WhatWeDo/Expertise.jsx
@@ -56,7 +56,7 @@ const Expertise = () => {
 
   return (
     <>
-      <div className="md:py-[80px] py-[22.95px] px-[15px] flex flex-col justify-center items-center gap-[5.15px] md:gap-[29.38px] w-full md:max-w-11/12">
+      <div className="md:py-[80px] py-[22.95px] flex flex-col justify-center items-center gap-[5.15px] md:gap-[29.38px] w-11/12 mx-auto">
         <div className="flex flex-col items-center">
           <p className="text-black md:text-[27.43px] font-[400] font-Inter text-[14.75px]">
             WHAT WE DO
@@ -65,11 +65,11 @@ const Expertise = () => {
             End-to-End Energy Expertise
           </h2>
         </div>
-        <div className="md:grid md:grid-cols-3 grid-rows-1 md:gap-[50.53px] gap-[10px] w-full md:max-w-10/12 flex overflow-x-auto">
+        <div className="md:grid md:grid-cols-3 grid-rows-1 gap-[25px] w-full flex overflow-x-auto">
           {services.map((service) => (
-            <div className="group text-center md:h-[557.32px] md:w-full w-11/12 rounded-[7.84px] md:border md:border-[0.98px] border-[#969797] flex-shrink-0 md:flex-shrink md:bg-white bg-[#E6F3EC] md:hover:bg-gray-50">
+            <div className="group text-center md:w-full w-11/12 rounded-[7.84px] md:border md:border-1 border-[#969797]  md:bg-white bg-[#E6F3EC] md:hover:bg-gray-50">
               <img src={service.image} alt="" className="w-full" />
-              <div className="flex flex-col gap-[26.45px] md:bg-white md:group-hover:bg-gray-50">
+              <div className="flex flex-col gap-[26.45px] md:bg-white md:group-hover:bg-gray-50 pb-4">
                 <h3 className="bg-black font-[700] text-white text-xl sm:text-2xl font-bold font-Inter group-hover:text-[#F6C200]">
                   {service.title}
                 </h3>
