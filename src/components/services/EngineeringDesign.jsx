fc0349fd06143d1f6feeb95c53dd6f11eee4587b
PraiseS-2 <praisegodakokadike@gmail.com>
2025-07-14 12:15:18 +0100

    forgot password done

--- src/components/services/EngineeringDesign.jsx
+++ src/components/services/EngineeringDesign.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 import grouppic from "../../assets/GroupPlanning.png";
 
 const EngineeringDesign = () => {
   return (
     <div className="bg-[#008A3F] md:flex md:align-middle md:justify-content-center md:w-full">
-      <div className=" pt-18 pb-16 px-10 text-[#FEFFFF] md:w-1/2 md:my-auto lg:mx-17 md:p-0 md:mx-10 md:w-1/2">
+      <div className=" pt-18 pb-16 px-10 text-[#FEFFFF] md:w-1/2 md:my-auto lg:mx-17 md:p-0 md:mx-10">
         <p className="text-[11px] font-medium font-Inter lg:text-[24px] md:text-[13px]">
           ENGINEERING AND PROJECT MANAGEMENT
         </p>
@@ -21,8 +21,6 @@ const EngineeringDesign = () => {
       </div>
     </div>
   );
-}
+};
 
 export default EngineeringDesign;
-
-
